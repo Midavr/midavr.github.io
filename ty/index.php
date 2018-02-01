c39206a8c23382e00a20aff67a31c4760beabc5c
Vadim <rvskneu@gmail.com>
2018-02-01 19:57:23 +0200

    Add files via upload

---
+++ ty/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+
+    <meta charset="UTF-8">
+    <title>Спасибо за заказ!</title>
+    <link rel="stylesheet" type="text/css" href="../../thankyou/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="../../thankyou/css/style.css">
+    <link rel="stylesheet" href="../../thankyou/css/stylenull.css">
+    <link rel="stylesheet" href="../../list/dop/dop.css">
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter47354010 = new Ya.Metrika2({
+                    id:47354010,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks2");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/47354010" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+</head>
+
+<body>
+
+<!-- Tovarro Sensor -->
+            <script type="text/javascript">
+                (function() {
+                    var d = document, w = window;
+                    w.MgSensorData = w.MgSensorData || [];
+                    w.MgSensorData.push({cid:342651, lng:"ru", nosafari:true, project: "a.tovarro.com"});
+                    var l = "a.tovarro.com";
+                    var n = d.getElementsByTagName("script")[0];
+                    var s = d.createElement("script");
+                    s.type = "text/javascript";
+                    s.async = true;
+                    var dt = !Date.now?new Date().valueOf():Date.now();
+                    s.src = "//" + l + "/mgsensor.js?d=" + dt;
+                    n.parentNode.insertBefore(s, n);
+                })();
+            </script>
+        <!-- /Tovarro Sensor -->
+<div class="container">
+    <div id="cont">
+    <p id="ty">Спасибо&nbsp;!!!</p>
+    <p class="ty">Ваш заказ успешно оформлен.</p>
+    <p class="ty">Наши менеджеры свяжутся с Вами в ближайшее время</p>
+    </div>
+</div>
+<!-- Блок дополнительных товаров -->
+
+<div class="dop">
+    <div class="dopblock" id="dopbl-charger">
+        <p class="titledop">Колонка Charge2+</p>
+        <a href="http://smart-shop.site/bl-charger/<?= '?'.$_SERVER['QUERY_STRING']; ?>" target="_blank" id="dopsl">Подробнее</a>
+    </div>
+    <div class="dopblock" id="dopcable">
+        <p class="titledop">Магнитный кабель</p>
+        <a href="http://smart-shop.site/cable/<?= '?'.$_SERVER['QUERY_STRING']; ?>" target="_blank" id="dopsl">Подробнее</a>
+    </div>
+    <div class="dopblock" id="doppowerbank">
+        <p class="titledop">Power Bank</p>
+        <a href="http://smart-shop.site/powerbank/<?= '?'.$_SERVER['QUERY_STRING']; ?>" target="_blank" id="dopsl">Подробнее</a>
+    </div>
+    <div class="dopblock" id="dopbeats">
+        <p class="titledop">Наушники Beats solo</p>
+        <a href="http://smart-shop.site/beats/<?= '?'.$_SERVER['QUERY_STRING']; ?>" target="_blank" id="dopsl">Подробнее</a>
+    </div>
+    <div class="dopblock" id="dopstarbucks">
+        <p class="titledop">Термокружка Starbucks</p>
+        <a href="http://smart-shop.site/starbucks/<?= '?'.$_SERVER['QUERY_STRING']; ?>" target="_blank" id="dopsl">Подробнее</a>
+    </div>
+    <div class="dopblock" id="dopmicrosd">
+        <p class="titledop">Карта памяти</p>
+        <p style="margin: 0;">Уточните у оператора</p>
+    </div>
+</div>
+
+<!-- Google Code for &#1089;&#1090;&#1088;&#1072;&#1085;&#1080;&#1094;&#1072; &#1089;&#1087;&#1072;&#1089;&#1080;&#1073;&#1086; Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 833715998;
+var google_conversion_label = "tqBECNCzn3gQnv7FjQM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/833715998/?label=tqBECNCzn3gQnv7FjQM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/833715998/?label=tqBECNCzn3gQnv7FjQM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+</body>
+</html>
